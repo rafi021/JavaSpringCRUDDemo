881cbb046582639693df040ba5441b445b808f08
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2024-05-10 12:30:08 +0600

    crud done

--- src/main/java/com/ibrahim/firstSpringApp/controllers/EmployeeController.java
+++ src/main/java/com/ibrahim/firstSpringApp/controllers/EmployeeController.java
@@ -3,9 +3,9 @@ package com.ibrahim.firstSpringApp.controllers;
 import com.ibrahim.firstSpringApp.models.Employee;
 import com.ibrahim.firstSpringApp.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
@@ -15,5 +15,25 @@ public class EmployeeController {
     @PostMapping(value = "/employee/create")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeService.createEmployee(employee);
+    }
+
+    @GetMapping(value = "/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping(value = "/employees/{id}")
+    public Employee getEmployee(@PathVariable(value = "id") Long id) {
+        return employeeService.getEmployee(id);
+    }
+
+    @DeleteMapping(value = "/employees/{id}")
+    public void deleteEmployee(@PathVariable(value = "id") Long id) {
+        employeeService.deleteEmployee(id);
+    }
+
+    @PutMapping(value = "/employees/{id}")
+    public Employee updateEmployee(@PathVariable(value = "id") Long id, @RequestBody Employee employee) {
+        return employeeService.updateEmployee(id, employee);
     }
 }
--- src/main/java/com/ibrahim/firstSpringApp/services/EmployeeService.java
+++ src/main/java/com/ibrahim/firstSpringApp/services/EmployeeService.java
@@ -5,6 +5,8 @@ import com.ibrahim.firstSpringApp.repositories.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmployeeService {
     @Autowired
@@ -12,5 +14,24 @@ public class EmployeeService {
 
     public Employee createEmployee(Employee employee) {
         return  employeeRepo.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee getEmployee(Long id) {
+        return employeeRepo.findById(id).orElseThrow();
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteById(id);
+    }
+
+    public Employee updateEmployee(Long id, Employee employee) {
+        Employee getEmp = this.getEmployee(id);
+        getEmp.setName(employee.getName());
+        getEmp.setAddress(employee.getAddress());
+        return employeeRepo.save(getEmp);
     }
 }
